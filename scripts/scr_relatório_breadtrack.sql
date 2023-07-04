b794faaaefe7948d553180384277213c452fbd8d
brunogkonzen <brunogkonzen@hotmail.com>
2023-07-04 10:51:41 -0300

    relatórios finalizados

--- scripts/scr_relatório_breadtrack.sql
+++ scripts/scr_relatório_breadtrack.sql
@@ -1,11 +1,11 @@
---1) Relação de clientes com o nome, sexo e idade em ordem crescente de nome. Relacionar somente clientes cadastrados antes de 2023;
+--1) Relat�rio de clientes com o nome, sexo e idade em ordem crescente de nome. Relacionar somente clientes cadastrados antes de 2023;
 SELECT nomcli, sexcli, EXTRACT(YEAR FROM AGE(datnascli)) AS idade
 FROM Cliente
 WHERE datcadcli < '2023-01-01'
 ORDER BY nomcli ASC;
 
 
---2) Relação de produtos(nome e descrição) vendidos nos meses pares de 2022. Ordene o relatório pelo nome do produto de forma ascendente;
+--2) Rela??o de produtos(nome e descri??o) vendidos nos meses pares de 2022. Ordene o relat�rio pelo nome do produto de forma ascendente;
 SELECT p.nompro, e.proenc
 FROM Produto p
 INNER JOIN Encomenda e ON p.codpro = e.codpro
@@ -14,7 +14,7 @@ WHERE EXTRACT(YEAR FROM e.datenc) = 2022
 ORDER BY p.nompro ASC;
 
 
---3) Relação dos top 10 produtos vendidos em 2023;
+--3) Rela??o dos top 10 produtos vendidos em 2023;
 SELECT p.nompro, SUM(v.qtdven) AS total_vendas
 FROM Produto p
 INNER JOIN ProdutoVenda pv ON p.codpro = pv.codpro
@@ -22,12 +22,12 @@ INNER JOIN Venda v ON pv.codven = v.codven
 WHERE EXTRACT(YEAR FROM v.datven) = 2023
 GROUP BY p.nompro
 ORDER BY total_vendas DESC
-LIMIT 10;	
+LIMIT 10;
 
---4) Relação de meses, quantidade totalde vendas, valor total de vendas pormês. Relacionar somente meses comquantidade de vendas acima de 100. Ordenar o relatório do mês com o maior valor(R$) em vendas para o mês com menos vendas.
+--4) Relac?o de meses, quantidade totalde vendas, valor total de vendas por m?s. Relacionar somente meses comquantidade de vendas acima de 100. Ordenar o relat�rio do m?s com o maior valor(R$) em vendas para o m?s com menos vendas.
 SELECT EXTRACT(MONTH FROM v.datven) AS mes, COUNT(*) AS quantidade_vendas, SUM(v.vlrven) AS valor_total_vendas
 FROM Venda v
-where vlrven > 100
+where vlrven > 10
 GROUP BY mes
 ORDER BY valor_total_vendas DESC, quantidade_vendas ASC;
 
