033b8726bbb4ae943809d69fbeda31c6baabcd41
Arthur-bk <arthurkochem12@gmail.com>
2023-11-23 20:37:53 -0300

    Script criação de usuarios, grupos e concessão de privilégios

---
+++ scripts/scr_cria_usuarios_breadtrack.sql
@@ -0,0 +1,20 @@
+
+create user Arthur with login createdb password '1';
+create user Bruno with login createdb password '2';
+create user ArthurG with login createdb password '3';
+create user Leo with login createdb password '4';
+
+create group consultsa;
+create group operador;
+
+grant select on funcionario to consulta;
+grant select, insert, delete, update on funcionario to operador;
+
+grant operador to Leo;
+grant consulta to Arthut;
+grant consulta to Bruno;
+grant consulta to ArthurG;
+
+
+
+
