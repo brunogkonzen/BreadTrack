a81726082c99350d6b76dea9aa90af1c273407e2
arthur-gruber <arthurcg21@gmail.com>
2023-11-28 16:09:48 -0300

    Retirado commit relatório

--- scripts/scr_relatório_breadtrack.sql
+++ scripts/scr_relatório_breadtrack.sql
@@ -5,10 +5,10 @@ FROM Cliente
 WHERE datcadcli < '2023-01-01'
 ORDER BY nomcli ASC;
 
-select * from view_clientes vc ;
+select * from view_clientes vc;
 
 --2) Rela??o de produtos(nome e descri??o) vendidos nos meses pares de 2022. Ordene o relat�rio pelo nome do produto de forma ascendente;
---CREATE VIEW view_vendas_meses_pares AS 
+CREATE VIEW view_vendas_meses_pares AS 
 SELECT p.nompro, e.proenc
 FROM Produto p
 INNER JOIN Encomenda e ON p.codpro = e.codpro
@@ -19,7 +19,7 @@ ORDER BY p.nompro ASC;
 select * from view_vendas_meses_pares;
 
 --3) Relação dos top 10 produtos vendidos em 2023;
---CREATE VIEW view_top10 AS 
+CREATE VIEW view_top10 AS 
 SELECT p.nompro, SUM(v.qtdven) AS total_vendas
 FROM Produto p
 INNER JOIN ProdutoVenda pv ON p.codpro = pv.codpro
@@ -32,7 +32,7 @@ LIMIT 10;
 select * from view_top10;
 
 --4) Relacão de meses, quantidade totalde vendas, valor total de vendas por mês. Relacionar somente meses com quantidade de vendas acima de 100. Ordenar o relat�rio do m?s com o maior valor(R$) em vendas para o m?s com menos vendas.
---CREATE VIEW view_mes_maior_venda AS 
+CREATE VIEW view_mes_maior_venda AS 
 SELECT EXTRACT(MONTH FROM v.datven) AS mes, COUNT(*) AS quantidade_vendas, SUM(v.vlrven) AS valor_total_vendas
 FROM Venda v
 where vlrven > 10
